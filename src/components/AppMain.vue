f0e5bb73b1cbe5c8ebbf10ffc5c9bd861eef4121
EsterSPR <web.esterspreafico@gmail.com>
2023-01-17 17:58:52 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+import {store} from '../store';
+
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
